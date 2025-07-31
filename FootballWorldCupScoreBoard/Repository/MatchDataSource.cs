cb7c66c86acae9eecebb106c502b798122e6f00b
Boris Keleman <boris.keleman@creaplus.com>
2025-07-31 09:30:11 +0200

    feat(be): Datasource implementation.

---
+++ FootballWorldCupScoreBoard/Repository/MatchDataSource.cs
@@ -0,0 +1,36 @@
+﻿using FootballWorldCupScoreBoard.Domain;
+using FootballWorldCupScoreBoard.Interface;
+using System.Collections.Concurrent;
+
+namespace FootballWorldCupScoreBoard.Repository
+{
+    public class MatchDataSource : IMatchDataSource
+    {
+        private readonly ConcurrentDictionary<Guid, Match> _matches;
+
+        public MatchDataSource()
+        {
+            _matches = new ConcurrentDictionary<Guid, Match>();
+        }
+
+        public bool Add(Match match)
+        {
+            return _matches.TryAdd(match.Id, match);
+        }
+
+        public IEnumerable<Match> GetAllMatches()
+        {
+            return _matches.Values;
+        }
+
+        public Match? GetMatch(Guid matchId)
+        {
+            return _matches.TryGetValue(matchId, out var match) ? match : null;
+        }
+
+        public bool Remove(Guid matchId)
+        {
+            return _matches.TryRemove(matchId, out _);
+        }
+    }
+}
---
+++ WorldCupScoreBoard.Tests/UnitTests/MatchDataSourceTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using FootballWorldCupScoreBoard.Domain;
+using FootballWorldCupScoreBoard.Interface;
+using FootballWorldCupScoreBoard.Repository;
+
+namespace WorldCupScoreBoard.Tests.UnitTests
+{
+    public class MatchDataSourceTests : MatchDataSourceDriver
+    {
+        [Fact]
+        public void Add_SuccessfullyAddedMatch_RetrunTrue()
+        {
+            var result = Sut.Add(Match);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Add_DuplicateMatch_ReturnsFalse()
+        {
+            SetupDuplicateMatch();
+
+            var result = Sut.Add(Match);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Remove_Success_ReturnTrue()
+        {
+            Sut.Add(Match);
+
+            var result = Sut.Remove(Match.Id);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Remove_NonExistingMatch_ReturnsFalse()
+        {
+            var result = Sut.Remove(Guid.NewGuid());
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void GetAllMatches_ReturnsAllMatches()
+        {
+            SetupMatches();
+
+            var result = Sut.GetAllMatches();
+
+            result.Should().HaveCount(2);
+            result.Should().ContainEquivalentOf(Match1);
+            result.Should().ContainEquivalentOf(Match2);
+        }
+    }
+
+    public class MatchDataSourceDriver
+    {
+        public Match Match { get; }
+        public IMatchDataSource Sut { get; }
+        public Match Match1 { get; private set; }
+        public Match Match2 { get; private set; }
+
+        public MatchDataSourceDriver()
+        {
+            Match = new Match(new Team("Argentina"), new Team("France"));
+            Sut = new MatchDataSource();
+        }
+
+        public void SetupDuplicateMatch()
+        {
+            Sut.Add(Match);
+        }
+
+        public void SetupMatches()
+        {
+            Match1 = new Match(new Team("Portugal"), new Team("Uruguay"));
+            Match2 = new Match(new Team("USA"), new Team("Mexico"));
+            Sut.Add(Match1);
+            Sut.Add(Match2);
+        }
+    }
+}
